26024e8631b4dc7648b4597b437b437eca18be19
Gabriel Boniolo <gabrielboniolo@gmail.com>
2024-01-11 10:53:49 -0300

    Projeto finalizado

---
+++ Operacoes.cs
@@ -0,0 +1,58 @@
+﻿namespace SimpleCalculator
+{
+    internal class Operacoes
+    {
+        public static void Adicao()
+        {
+            Console.Clear();
+            Console.Write("Digite o primeiro número: ");
+            int numeroUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            int numeroDois = int.Parse(Console.ReadLine());
+            int resultado = numeroUm + numeroDois;
+            Console.WriteLine($"A soma entre {numeroUm} e {numeroDois} é igual a {resultado}");
+            Console.Write("Pressione qualquer tecla para prosseguir ");
+            Console.ReadKey();
+            Program.TelaFinal();
+        }
+        public static void Subtracao()
+        {
+            Console.Clear();
+            Console.Write("Digite o primeiro número: ");
+            int numeroUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            int numeroDois = int.Parse(Console.ReadLine());
+            int resultado = numeroUm - numeroDois;
+            Console.WriteLine($"A subtração entre {numeroUm} e {numeroDois} é igual a {resultado}");
+            Console.Write("Pressione qualquer tecla para prosseguir ");
+            Console.ReadKey();
+            Program.TelaFinal();
+        }
+        public static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.Write("Digite o primeiro número: ");
+            int numeroUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            int numeroDois = int.Parse(Console.ReadLine());
+            int resultado = numeroUm * numeroDois;
+            Console.WriteLine($"A multiplicação entre {numeroUm} e {numeroDois} é igual a {resultado}");
+            Console.Write("Pressione qualquer tecla para prosseguir ");
+            Console.ReadKey();
+            Program.TelaFinal();
+        }
+        public static void Divisao()
+        {
+            Console.Clear();
+            Console.Write("Digite o primeiro número: ");
+            double numeroUm = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            double numeroDois = int.Parse(Console.ReadLine());
+            double resultado = numeroUm / numeroDois;
+            Console.WriteLine($"A divisão entre {numeroUm} e {numeroDois} é igual a {resultado:F2}");
+            Console.Write("Pressione qualquer tecla para prosseguir ");
+            Console.ReadKey();
+            Program.TelaFinal();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TelaInicial();
+        }
+        public static void TelaInicial()
+        {
+            Console.WriteLine("Olá, bem-vindo a calculadora simples!");
+            Console.Write("Pressione qualquer tecla para começar ");
+            Console.ReadKey();
+            Principal();
+
+        }
+        public static void Principal()
+        {
+            Console.Clear();
+            Console.WriteLine("Escolha uma operação:\na - adição\ns - subtração\nm - multiplicação\nd - divisão");
+            Console.Write("Digite aqui sua respota: ");
+            char operacao = char.Parse(Console.ReadLine());
+            switch(operacao)
+            {
+                case 'a':
+                    Operacoes.Adicao();
+                    break;
+                case 's':
+                    Operacoes.Subtracao();
+                    break;
+                case 'm':
+                    Operacoes.Multiplicacao();
+                    break;
+                case 'd':
+                    Operacoes.Divisao();
+                    break;
+                default:
+                    Console.Write("Por favor digite a letra correspondente (a/s/m/d) ");
+                    Console.ReadKey();
+                    Principal();
+                    break;
+            }
+        }
+        public static void TelaFinal()
+        {
+            Console.Clear();
+            Console.Write("Obrigado por utilizar o programa! Deseja realizar outra operação? (s/n): ");
+            char resposta = char.Parse(Console.ReadLine());
+            if(resposta == 's')
+                Principal();
+            else
+                Environment.Exit(0);
+        }
+    }
+}
